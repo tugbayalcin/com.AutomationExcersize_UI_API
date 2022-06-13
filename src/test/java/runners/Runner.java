a0a1c8ae8ef2f2425a5751fae5e22f91b681dd9b
Suat ORUC <93098605+suatoruc@users.noreply.github.com>
2022-06-13 18:10:57 +0300

    V1

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt"},
         features ="src/test/resources/features",
-        glue = "stepdefinitions",
+        glue = {"stepdefinitions","hooks"},
         tags = "tag",
         dryRun = false
 )
